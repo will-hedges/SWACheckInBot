98988243dcf9bcdafa3b5ca9b95a1a094613a6ca
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2021-03-06 13:38:27 -0700

    Removed class init fn, improved validation
    * added date, time formats
    * added regex validation for confirmation # and name
    * validated date not in past

--- southwest_checkin.py
+++ southwest_checkin.py
@@ -2,9 +2,8 @@
 # southwest_checkin.py - DESCRIPTION
 
 
-from datetime import date
+from datetime import date, datetime, time
 import logging
-import time
 
 import pyinputplus as pyip
 from selenium import webdriver
@@ -18,38 +17,60 @@ logging.basicConfig(
 
 class Reservation():
 
-    def __init__(self):
-        self.current_year = date.today().year
-        return
-
-
     def get_checkin_date(self):
-        checkin_date = pyip.inputDate(
-            prompt='Check-in date (MM/DD): ',
-            formats=['%m/%d']
-            )
-        checkin_date = date(self.current_year, checkin_date.month, checkin_date.day)
-        self.checkin_date = checkin_date
-        return
+        while True:
+            self.checkin_date = pyip.inputDate(
+                prompt='Check-in date: ',
+                formats=[
+                    '%m/%d', '%m/%d/%y', '%m/%d/%Y',
+                    '%m.%d', '%m.%d.%y', '%m.%d.%Y',
+                    '%m-%d', '%m-%d-%y', '%m.%d.%Y',
+                    '%d-%m', '%d-%m-%y', '%d-%m-%Y',
+
+                    # TODO add more formats, ISO, etc.
+
+                    ]
+                )
+
+            # TODO default to current year if none provided
+
+            if self.checkin_date < date.today():
+                print('Check-in date cannot be in the past.')
+            else:
+                return
 
 
     def get_checkin_time(self):
         checkin_time = pyip.inputTime(
-            prompt='Check-in time (HH:MM AM/PM): ',
-            formats=['%I:%M %p']
+            prompt='Check-in time: ',
+            formats=[
+                '%H%M', '%H:%M',
+                '%I:%M', '%I:%M%p', '%I:%M %p'
+                ]
             )
+
+        # TODO validate input for AM/PM (period)
+        # TODO validate that checkin datetime is not in the past
+
         self.checkin_time = checkin_time
         return
 
 
-    def get_confirmation(self):
-        confirmation = pyip.inputStr(prompt='Confirmation number: ')
-        self.confirmation = confirmation
+    def get_confirmation_num(self):
+        self.confirmation_num = pyip.inputStr(
+            prompt='Confirmation #: ',
+            blockRegexes=[r'.*'],
+            allowRegexes=[r'^(\w{6})$']
+            )
         return
 
 
     def get_ticket_name(self):
-        name = pyip.inputStr(prompt='Reservation name: ').split()
+        name = pyip.inputStr(
+            prompt='Reservation name: ',
+            blockRegexes=[r'.*'],
+            allowRegexes=[r'^[A-Za-z]+\s[A-Za-z]$']
+            ).split()
         self.firstname = name[0]
         self.lastname = name[1]
         return
@@ -58,12 +79,11 @@ class Reservation():
     def get_reservation(self):
         self.get_checkin_date()
         self.get_checkin_time()
-        self.get_confirmation()
+        self.get_confirmation_num()
         self.get_ticket_name()
 
 
     def confirm_reservation(self):
-        # TODO review all stored variables
         pass
 
 
